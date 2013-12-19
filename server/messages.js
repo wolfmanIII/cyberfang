c3c23bd721a31307254fb6c6e99de62b2e04a486
wolfmanIII <wolfmanIII@gmail.com>
2013-12-19 18:47:58 +0100

    auth on update

--- server/messages.js
+++ server/messages.js
@@ -1,3 +1,9 @@
 Meteor.publish("all-messages", function () {
 	return Messages.find();
 });
+
+Messages.allow({
+        insert: function () { return true; },
+        update: function () { return true; },
+        remove: function () { return true; } 
+      });
